3f8ec33246931cb6249bd853380aecc4e488c3d3
dannynator <d.seledtsova@gmail.com>
2018-01-21 05:11:16 +0100

    jo dudes wir habens

--- app/src/main/java/com/billigeplaetze/atm4vi/domain/uc/PhotoTakenInteractor.java
+++ app/src/main/java/com/billigeplaetze/atm4vi/domain/uc/PhotoTakenInteractor.java
@@ -3,6 +3,7 @@ package com.billigeplaetze.atm4vi.domain.uc;
 import android.content.Context;
 
 import com.billigeplaetze.atm4vi.domain.ScreenRecognizer;
+import com.billigeplaetze.atm4vi.domain.definitions.IScreenChangedListener;
 import com.billigeplaetze.atm4vi.domain.definitions.OCRService;
 import com.billigeplaetze.atm4vi.services.ocr.pojo.ReceivedData;
 
@@ -15,15 +16,18 @@ import java.io.InputStream;
 public class PhotoTakenInteractor implements IPhotoTakenUseCase {
     private final OCRService ocrService;
     private final Context context;
-    public PhotoTakenInteractor(OCRService ocrService, Context context) {
+    private final IScreenChangedListener listener;
+
+    public PhotoTakenInteractor(OCRService ocrService, Context context, IScreenChangedListener listener) {
         this.ocrService = ocrService;
         this.context = context;
+        this.listener = listener;
     }
 
     @Override
     public void submitPhoto(InputStream photo) {
          ReceivedData receivedData = ocrService.receiveImageData(photo);
-         ScreenRecognizer recognizer = new ScreenRecognizer(receivedData, bob);
+         ScreenRecognizer recognizer = new ScreenRecognizer(receivedData, listener);
          ScreenRecognizer.appendLog("Screen Nr.:" + recognizer.recognize());
 
     }
---
+++ app/src/main/java/com/billigeplaetze/atm4vi/domain/uc/ScreenChangedInteractor.java
@@ -0,0 +1,56 @@
+package com.billigeplaetze.atm4vi.domain.uc;
+
+import android.content.Context;
+
+import com.billigeplaetze.atm4vi.R;
+import com.billigeplaetze.atm4vi.domain.definitions.IScreenChangedListener;
+import com.billigeplaetze.atm4vi.domain.definitions.ITTSEngine;
+import com.billigeplaetze.atm4vi.domain.definitions.Screen;
+
+/**
+ * Created by dannynator on 21.01.18.
+ */
+
+public class ScreenChangedInteractor implements IScreenChangedListener {
+
+    private ITTSEngine ittsEngine;
+    private Context context;
+
+    public ScreenChangedInteractor(ITTSEngine ittsEngine, Context context) {
+        this.ittsEngine = ittsEngine;
+        this.context = context;
+        ittsEngine.initialize(context);
+    }
+
+    @Override
+    public void onScreenChanged(Screen screen) {
+        switch (screen) {
+            case PIN:
+                ittsEngine.readOut(context.getResources().getString(R.string.enterPin));
+                break;
+            case Welcome:
+                ittsEngine.readOut(context.getResources().getString(R.string.insertCard));
+                break;
+            case Functions:
+                String functions = context.getResources().getString(R.string.functionsScreen);
+                functions += " " + context.getResources().getString(R.string.buttonL2);
+                functions += " " + context.getResources().getString(R.string.buttonR1);
+                ittsEngine.readOut(functions);
+                break;
+            case MoneyMoney:
+                String moneyTakin = context.getResources().getString(R.string.functionsScreen);
+                moneyTakin += context.getResources().getString(R.string.buttonML1);
+                moneyTakin += context.getResources().getString(R.string.buttonDown) + context.getResources().getString(R.string.buttonML2);
+                moneyTakin += context.getResources().getString(R.string.buttonDown) + context.getResources().getString(R.string.buttonML3);
+                moneyTakin += context.getResources().getString(R.string.buttonDown) + context.getResources().getString(R.string.buttonML4);
+                moneyTakin += context.getResources().getString(R.string.buttonR1);
+                moneyTakin += context.getResources().getString(R.string.buttonDown) + context.getResources().getString(R.string.buttonMR2);
+                moneyTakin += context.getResources().getString(R.string.buttonDown) + context.getResources().getString(R.string.buttonMR3);
+                ittsEngine.readOut(moneyTakin);
+                break;
+            default:
+                ittsEngine.readOut("Kein Geldautomat erkannt");
+                break;
+        }
+    }
+}
